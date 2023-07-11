bbfdd0a3baf3be354ca4a6fa7bb9f1dd9f76422c
FromLiQg <1501077298@qq.com>
2023-07-11 16:32:42 +0800

    add license headers

--- app-framework/app_ext_lib_export.c
+++ app-framework/app_ext_lib_export.c
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2019 Intel Corporation.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
 #include "lib_export.h"
 
 #ifdef APP_FRAMEWORK_SENSOR
